0c9d4d8d119cdd1df9b820fc6526ce8c8e809911
jackdono <jack.donohue1@gmail.com>
2021-11-22 17:48:32 +1000

    Fixed geopy timeout for ambiguous addresses

--- directions.py
+++ directions.py
@@ -7,8 +7,14 @@ geolocator = Nominatim(user_agent="SMS-Directions")
 
 def getroute(start_addr, end_addr):
 
-    start_loc = geolocator.geocode(str(start_addr))
-    end_loc = geolocator.geocode(str(end_addr))
+    try:
+        start_loc = geolocator.geocode(str(start_addr))
+        end_loc = geolocator.geocode(str(end_addr))
+    except:
+        # Timeout occurs if an address isn't specific enough
+        route_string = 'One or both addresses are not specific enough. Are you missing a suburb?'
+        return route_string
+    
 
     # Attempt to resolve coordinates and route
     try:
@@ -18,30 +24,28 @@ def getroute(start_addr, end_addr):
         route_found = True
     except:
         # Route not found or addresses invalid (or api key missing)
-        route_found = False
+        route_string = 'No route was found. Double check the addresses.'
+        return route_string
         
 
-    if route_found:
-        # Segments (nested dict inside 'routes') contains steps, waypoints etc.
-        # Get total distance and time
-        segments_dict = (routes.get('routes')[0]).get('segments')[0]
-        dist_total = round(segments_dict.get('distance')/1000, 1)
-        time_total = round(segments_dict.get('duration')/60, 1)
-
-
-        route_string = str(dist_total) + ' km, ' + str(time_total) + ' min (driving)\n'
-
-
-        # Steps - each step is its own list element, stored as a dictionary with distances, times, instructions etc.
-        steps_list = segments_dict.get('steps')
-
-        for i in range(len(steps_list)):
-            step = steps_list[i]
-            instruction = step.get('instruction')
-            distance = str(round(step.get('distance')/1000, 2)) + ' km'
-            route_string += instruction + ', go ' + distance + '\n\n'
-
-    elif not route_found:
-        route_string = 'No route was found. Double check the addresses.'
     
+    # Segments (nested dict inside 'routes') contains steps, waypoints etc.
+    # Get total distance and time
+    segments_dict = (routes.get('routes')[0]).get('segments')[0]
+    dist_total = round(segments_dict.get('distance')/1000, 1)
+    time_total = round(segments_dict.get('duration')/60, 1)
+
+    route_string = str(dist_total) + ' km, ' + str(time_total) + ' min (driving)\n'
+
+
+    # Steps - each step is its own list element, stored as a dictionary with distances, times, instructions etc.
+    steps_list = segments_dict.get('steps')
+
+    for i in range(len(steps_list)):
+        step = steps_list[i]
+        instruction = step.get('instruction')
+        distance = str(round(step.get('distance')/1000, 2)) + ' km'
+        route_string += instruction + ', go ' + distance + '\n\n'
+      
     return route_string
+
